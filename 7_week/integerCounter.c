9959b83e1fb3d952a22c76fff9354cf662db65db
Emmet Murray <e.minor.murray@gmail.com>
2018-04-18 17:09:50 +1000

    Start week 7

---
+++ 7_week/integerCounter.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main (void) {
+    int x;
+ 
+    printf("Please enter a number between 1 and 99: ");
+
+    while (scanf("%d", &x) == 1) {
+    }
+
+    return 0;
+}
---
+++ 7_week/uppser_case.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void) {
+
+    char c;
+
+    while () {
+    }
+
+    return 0;
+}
