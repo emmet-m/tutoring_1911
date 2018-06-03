9efbe3dfc905b66ee3dcb0c49706c7ac8bbac51d
Emmet Murray <e.minor.murray@gmail.com>
2018-06-03 23:10:54 +1000

    Struct stuff

---
+++ revision_day/structs_and_pointers/design_struct.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// TODO: Implement these functions.
+// If you want to change the return types/paramaters, you can!
+
+// Reads a student profile in from stdin, returning a new StudentProfile
+// Check studentData.txt for an example of the data format.
+StudentProfile readStudent();
+
+// Prints a student profile back out (in the same format as read in)
+void printStudent(StudentProfile p);
+
+// Deletes a student profile.
+// If you use malloc anywhere in readStudent, you will need to 
+// free it here.
+void destroyStudent(StudentProfile p);
+
+/* A student profile should contain:
+ * 1) a name
+ * 2) a zid
+ * 3) a WAM
+ * 4) an array of current subjects (e.g. COMP1911, ENGG1000, ...)
+ *
+ * Assume a student will always have 4 current subjects.
+ *
+ * Give the following struct fields that could hold all these values
+ *
+ * The types of these values are up to you! Just make sure they can
+ * correctly hold the data.
+ */
+
+// You can change this definition if you wish
+typedef struct studentProfile StudentProfile;
+
+struct studentProfile {
+    // TODO: fill this in
+};
+
+int main (void) {
+
+    // Test your functions here
+
+    return 0;
+}
+
+StudentProfile readStudent(){
+    StudentProfile p;
+    // TODO
+    return p;
+}
+
+void printStudent(StudentProfile p) {
+    // TODO
+}
+
+void destroyStudent(StudentProfile p) {
+    // TODO
+}
---
+++ revision_day/structs_and_pointers/make_struct.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// Make sure to change this prototype too!
+void makeData(int requestedSize);
+
+// TODO: Write a typedef here for the below struct
+struct data {
+    int size;
+    double * text;
+};
+
+int main (void) {
+
+    // replace these definitions with the typedef you create above
+    struct data * myData = NULL;
+
+    // TODO: Here, use makeData to initialise the above variable
+
+    checkStruct(myData);
+
+    return 0;
+}
+
+// The return type of this function may not work for what we need...
+// Can we fix it?
+void makeData(int requestedSize) {
+
+    // This function should:
+    // 1) Make a new struct data
+    // 2) Initialise the array and size in it
+    // 3) Fill it with zeroes
+    // 4) Return a pointer to it
+
+}
+
+void checkStruct(struct * data input) {
+    int i = 0;
+    for (; i < input->size; i++) {
+        if (input->text[i] != 0) {
+            pritnf("Error: struct array not intialised to 0!");
+            exit(1);
+        }
+    }
+} 
+
+
