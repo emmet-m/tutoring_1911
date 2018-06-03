31425b6e8dfbc5b8acf714df1922c6c6a08296a1
Emmet Murray <e.minor.murray@gmail.com>
2018-06-03 23:51:01 +1000

    One more prac question

---
+++ revision_day/practice_exam_questions/numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) {
+    
+    /*
+     * Read in up to 100 integers, until no more numbers can be read
+     * Ignore all integers that are divisible by 5
+     * print the mean, median and mode of the remaining numbers
+     */
+
+    return 0;
+}
