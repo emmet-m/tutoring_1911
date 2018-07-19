ef01e0a8699c4adf6f5c1bdcd3860049b62c4859
Emmet Murray <e.minor.murray@gmail.com>
2018-07-19 20:44:20 +1000

    Added recursive insertOrdered.c solution

--- bridging_course/tuesday/insertOrdered.c
+++ bridging_course/tuesday/insertOrdered.c
@@ -6,25 +6,37 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 struct node {
     int       data;
     struct node *next;
 };
 
+struct node * recursive(int item, struct node * list);
+struct node * iterative(int item, struct node * list);
+
 int main (void) {
-    struct node * n = malloc(sizeof(struct node));
-    struct node * m = malloc(sizeof(struct node));
-    struct node * k = malloc(sizeof(struct node));
+    // Insert into empty list
+    struct node * n = recursive(10, NULL);
+    // List: 10->X
 
-    n->next = m;
-    m->next = k;
-    k->next = NULL;
+    n = recursive(15, n);
+    // List: 10->15->X
 
-    n->data = 10;
-    m->data = 5;
-    k->data = 0;
+    n = recursive(5, n);
+    // List: 5->10->15->X
 
+    // These are very bad tests...
+    // TODO: As an exercise, you should turn these into black box tests!
+    // You may need to include some extra functions.
+
+    printf("Testing recursive insertion works...\n");
+    assert(n->data == 5);
+    assert(n->next->data == 10);
+    assert(n->next->next->data == 15);
+    assert(n->next->next->next == NULL);
+    printf("Test passed!\n");
 
     return EXIT_SUCCESS;
 }
@@ -57,5 +69,29 @@ struct node * iterative(int item, struct node * list) {
 }
 
 struct node * recursive(int item, struct node * list) {
-    return NULL;
+    // Empty list or insert at the end of list
+    if (list == NULL) {
+        struct node * n = malloc(sizeof(struct node));
+        n->next = NULL;
+        n->data = item;
+        return n;
+    }
+    
+    // If current node in list is bigger than 
+    // the item to insert... i.e. we need to insert
+    // BEFORE current node
+    if (list->data > item) {
+        struct node * n = malloc(sizeof(struct node));
+        n->next = list; // Note here, we connect it to the current level node
+        n->data = item;
+
+        // This will be connected to the previous node (if there is one)
+        // below.
+        return n; 
+    }
+
+    // Insert after one of the following nodes, set the next
+    // connection of this node to be a recursive call
+    list->next = recursive(item, list->next);
+    return list;
 }
